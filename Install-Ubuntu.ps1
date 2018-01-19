ffe95db3adcc21eed376a16df99a9c9f6f62f374
Thomas Jaekel <Wilhelm.Wien@outlook.de>
2018-01-19 17:20:23 +0100

    Initial commit

---
+++ Install-Ubuntu.ps1
@@ -0,0 +1,12 @@
+﻿$ComputerName = "U1"
+
+$VmName = "$Lab-$ComputerName"
+New-LabVm -ComputerName $ComputerName
+
+Add-VMDvdDrive -VMName $VmName
+$Iso = "C:\iso\ubuntu-16.04-desktop-amd64.iso"
+Set-VMDvdDrive -VMName $VmName -Path $Iso
+Set-VMFirmware -VMName $VmName -FirstBootDevice (Get-VMDvdDrive -VMName $VmName)
+Set-VMFirmware -VMName $VmName -SecureBootTemplate "MicrosoftUEFICertificateAuthority"
+
+Connect-LabVm -ComputerName $ComputerName
