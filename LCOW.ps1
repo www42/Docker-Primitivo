6bdd0f0c04428821ebc391a8a42136b123738580
Thomas Jaekel <Wilhelm.Wien@outlook.de>
2018-01-20 21:12:07 +0100

    Upload new files

---
+++ LCOW.ps1
@@ -0,0 +1,71 @@
+﻿#region Prerequisites
+
+# Directory C:\Program Files\Linux Containers
+# At the end of the day there should be three files:
+#
+#        bootx64.efi   (from LinuxKit)
+#        initrd.img    (from LinuxKit)
+#        uvm.vhdx  
+
+
+$dir = "$Env:ProgramFiles\Linux Containers”
+mkdir $dir
+
+# Set special permission
+function set_perms {
+  param([string] $Root)
+  # Give the virtual machines group full control
+  $acl = Get-Acl -Path $Root
+  $vmGroupRule = new-object System.Security.AccessControl.FileSystemAccessRule("NT VIRTUAL MACHINE\Virtual Machines", "FullControl","ContainerInherit,ObjectInherit", "None", "Allow")
+  $acl.SetAccessRule($vmGroupRule)
+  Set-Acl -AclObject $acl -Path $Root
+}
+set_perms -Root  $dir
+
+# Download tool to uncompress .xz file
+curl https://tukaani.org/xz/xz-5.2.3-windows.zip -OutFile "$HOME\Downloads\xz-5.2.3-windows.zip"
+Expand-Archive -Path "$HOME\Downloads\xz-5.2.3-windows.zip" -DestinationPath "$HOME\Downloads\xz-5.2.3-windows"
+$xz = "$HOME\Downloads\xz-5.2.3-windows\bin_x86-64\xz.exe"
+
+#endregion
+
+#region Download LinuxKit
+
+# Inspect source
+Start-Process "https://github.com/friism/linuxkit/releases"
+
+$Uri = "https://github.com/friism/linuxkit/releases/download/preview-1/linuxkit.zip"
+Invoke-WebRequest -UseBasicParsing -Uri $Uri -OutFile $HOME\Downloads\linuxkit.zip
+
+Expand-Archive $HOME\Downloads\linuxkit.zip -DestinationPath "$dir\." 
+
+ #endregion 
+
+ #region Download vhdx
+
+# Inspect source
+Start-Process -FilePath "https://partner-images.canonical.com/hyper-v/linux-containers/xenial/current"
+
+$Uri = "https://partner-images.canonical.com/hyper-v/linux-containers/xenial/current/xenial-container-hyperv.vhdx.xz"
+Invoke-WebRequest -UseBasicParsing -Uri $Uri -OutFile $HOME\Downloads\xenial-container-hyperv.vhdx.xz
+
+& $xz -d $HOME\Downloads\xenial-container-hyperv.vhdx.xz
+dir "$HOME\Downloads\xenial-container-hyperv.vhdx"
+
+Copy-Item -Path "$HOME\Downloads\xenial-container-hyperv.vhdx" -Destination "$dir\uvm.vhdx"
+
+#endregion
+
+#region Start Docker daemon -- Start new Powershell console (not ISE)
+
+$env:LCOW_SUPPORTED=1
+$env:LCOW_API_PLATFORM_IF_OMITTED="linux"
+C:\Labs\Docker\LCOW\dockerd.exe -D --experimental -H "npipe:////./pipe//docker_lcow" --data-root C:\Labs\Docker\LCOW\data-root
+
+#endregion
+
+#region Try it -- Start new Powershell console (not ISE)
+
+$env:DOCKER_HOST = "npipe:////./pipe//docker_lcow"
+
+#endregion
---
+++ Test-Docker at WS Insider Build 17074.ps1
@@ -0,0 +1,44 @@
+﻿$ComputerName = "SVR2"
+
+New-LabVm -ComputerName $ComputerName
+
+$Iso = "D:\iso\Windows_InsiderPreview_Server_17074.iso"
+$VmName = "$Lab-$ComputerName"
+Add-VMDvdDrive -VMName $VmName
+Set-VMDvdDrive -VMName $VmName -Path $Iso
+Set-VMFirmware -VMName $VmName -FirstBootDevice (Get-VMDvdDrive -VMName $VmName)
+
+Connect-LabVm -ComputerName $ComputerName
+
+Set-VMProcessor -VMName $VmName -ExposeVirtualizationExtensions $true
+Set-VMMemory -VMName $VmName -StartupBytes 10GB
+Set-VMNetworkAdapter -VMName $VmName -MacAddressSpoofing On
+
+$LocalCred = New-Object -TypeName System.Management.Automation.PSCredential 'Administrator',(ConvertTo-SecureString $LabPw -AsPlainText -Force)
+
+$SVR2 = New-PSSession -VMName $VmName -Credential $LocalCred
+Enter-PSSession $SVR2
+
+Install-WindowsFeature -Name Containers,Hyper-V -IncludeManagementTools -Restart
+dir "$env:ProgramFiles\Linux Containers"
+
+Get-PackageProvider -ListAvailable
+Find-PackageProvider -Name "*docker*"
+Install-PackageProvider -Name "DockerMsftProviderInsider" -Force
+Find-Package -ProviderName "DockerMsftProviderInsider"
+Install-Package -Name "Docker" -ProviderName "DockerMsftProviderInsider" -Force
+Restart-Computer
+# we got 17.06.2-ee-6
+# docker pull    microsoft/nanoserver-insider       ... ok
+# docker run ... microsoft/nanoserver-insider       ... error
+
+Uninstall-Package docker
+
+# try 2.0.0-ee-1-tp6 (17.06.3-ee-1-tp6)
+Install-PackageProvider -Name "DockerProvider" -Force
+Find-Package -ProviderName "DockerProvider"
+Find-Package -ProviderName "DockerProvider" -RequiredVersion "preview"
+Install-Package -Name "Docker" -RequiredVersion "preview" -ProviderName "DockerProvider" -Force
+
+# isolation hyperv funktioniert
+docker run --rm -ti --isolation hyperv microsoft/nanoserver-insider cmd
