a4bd0a0078797642357e06659b64610f03df95d1
Thomas Jaekel <Wilhelm.Wien@outlook.de>
2018-01-22 22:32:53 +0100

    nested virt

--- Sneak peek at LCOW.ps1
+++ Sneak peek at LCOW.ps1
@@ -5,3 +5,9 @@ $LcowPath = "C:\Labs\Docker\LCOW_20180122"
 mkdir $LcowPath
 Invoke-WebRequest -UseBasicParsing -OutFile "$LcowPath\dockerd.exe" https://master.dockerproject.org/windows/x86_64/dockerd.exe
 Invoke-WebRequest -UseBasicParsing -OutFile "$LcowPath\docker.exe" https://master.dockerproject.org/windows/x86_64/docker.exe
+
+
+Set-VMProcessor -VMName Windows_10 -ExposeVirtualizationExtensions $true
+Set-VMMemory -VMName Windows_10 -StartupBytes 4GB
+Set-VMNetworkAdapter -VMName Windows_10 -MacAddressSpoofing On
+Start-VM Windows_10
